96595f3a87b00da2d4b964f003ae5bffbf5b248f
Tran Phat Trien <trientranphat@gmail.com>
2023-11-23 16:53:35 +0700

    Event

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -30,7 +30,7 @@ const Events = () => {
                   </div>
                 </div>
               </div> */}
-              <div className={`${styles.section} my-8`}>
+              <div className={`${styles.section}`}>
                 <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-4 xl:gap-[30px] mb-12">
                   {allEvents.map((allEvents, index) => (
                     // <EventCard key={index} data={allEvents} />
