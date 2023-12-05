7412ed8ab26594d2454ed90279fa25ee49090426
Tran Phat Trien <trientranphat@gmail.com>
2023-12-05 11:03:08 +0700

    update events.jsx

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -41,7 +41,7 @@ const Events = () => {
               </div> */}
               <div className={`${styles.section}`}>
                 <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-4 xl:gap-[30px] mb-12">
-                  {allEvents.map((allEvents, index) => (
+                  {allEvents.slice(0, 4).map((allEvents, index) => (
                     // <EventCard key={index} data={allEvents} />
                     <EventCard key={index} data={allEvents} />
                   ))}{" "}
